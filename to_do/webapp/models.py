dbbbedf3660cee3e1eb630680929b65afd11f9d8
Nazira <rysbekova2299@gmail.com>
2023-06-20 03:13:29 +0600

    добавила модели

--- to_do/webapp/models.py
+++ to_do/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+class List(models.Model):
+
+    description = models.TextField(max_length=2000, null=False, blank=False)
+    status = models.CharField(max_length=100, null=False, blank=False, choices=status_choices)
+    date = models.DateField(verbose_name="Дата выполнения")
+
+
+
+    def __str__(self):
+        return f"{self.pk} {self.description}: {self.status}"
+
